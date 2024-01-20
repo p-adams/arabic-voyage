1e1c49c002d2f685113ca2ca7369408842a8b72f
Paul Adams <ghusiyy@gmail.com>
2024-01-19 21:15:52 -0800

    add alphabet data

--- src/app/alphabet/page.tsx
+++ src/app/alphabet/page.tsx
@@ -1,7 +1,18 @@
+import { arabicAlphabet } from "@/data";
+import "./page.css";
+
 function AlphabetModule() {
   return (
-    <section>
+    <section className="alphabet-module">
       <h1>Alphabet Module</h1>
+      <div className="alphabet-cards">
+        {arabicAlphabet.map((ch, i) => (
+          <div key={ch.arabicChar} className="alphabet-card">
+            <div>{ch.arabicChar}</div>
+            <div>{ch.transliteration}</div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/data/index.ts
@@ -0,0 +1,36 @@
+export interface ArabicAlphabet {
+  arabicChar: string;
+  transliteration: string;
+}
+
+export const arabicAlphabet: ArabicAlphabet[] = [
+  { arabicChar: "ا", transliteration: "alif" },
+  { arabicChar: "ب", transliteration: "ba" },
+  { arabicChar: "ت", transliteration: "ta" },
+  { arabicChar: "ث", transliteration: "tha" },
+  { arabicChar: "ج", transliteration: "jim" },
+  { arabicChar: "ح", transliteration: "ha" },
+  { arabicChar: "خ", transliteration: "kha" },
+  { arabicChar: "د", transliteration: "dal" },
+  { arabicChar: "ذ", transliteration: "dhal" },
+  { arabicChar: "ر", transliteration: "ra" },
+  { arabicChar: "ز", transliteration: "za" },
+  { arabicChar: "س", transliteration: "sin" },
+  { arabicChar: "ش", transliteration: "shin" },
+  { arabicChar: "ص", transliteration: "sad" },
+  { arabicChar: "ض", transliteration: "dad" },
+  { arabicChar: "ط", transliteration: "taa" },
+  { arabicChar: "ظ", transliteration: "zaa" },
+  { arabicChar: "ع", transliteration: "ain" },
+  { arabicChar: "غ", transliteration: "ghain" },
+  { arabicChar: "ف", transliteration: "fa" },
+  { arabicChar: "ق", transliteration: "qaf" },
+  { arabicChar: "ك", transliteration: "kaf" },
+  { arabicChar: "ل", transliteration: "lam" },
+  { arabicChar: "م", transliteration: "mim" },
+  { arabicChar: "ن", transliteration: "nun" },
+  { arabicChar: "ه", transliteration: "haa" },
+  { arabicChar: "و", transliteration: "waw" },
+  { arabicChar: "ي", transliteration: "ya" },
+  { arabicChar: "ء", transliteration: "hamza" },
+];
